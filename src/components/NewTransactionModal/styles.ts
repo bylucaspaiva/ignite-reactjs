5787a388e0868cb14fcb4ba632c54c595034ccd8
lspaiva <lucasp4iva@gmail.com>
2022-03-08 13:34:37 -0300

    style: finishing styling in modal

--- src/components/NewTransactionModal/styles.ts
+++ src/components/NewTransactionModal/styles.ts
@@ -18,5 +18,31 @@ export const Container = styled.form`
 
     font-weight: 400;
     font-size: 1rem;
+
+    &::placeholder {
+      color: var(--text-body);
+    }
+
+    & + input {
+      margin-top: 1rem;
+    }
+  }
+
+  button[type="submit"] {
+      width: 100%;
+      padding: 0 1.5rem;
+      height: 4rem;
+      background: var(--green);
+      color: #fff;
+      border-radius: 0.25rem;
+      border: 0;
+      font-size: 1rem;
+      margin-top: 1.5rem;
+      font-weight: 600;
+
+      transition: filter 0.2s;
+      &:hover {
+        filter: brightness(0.9);
+      }
   }
 `
