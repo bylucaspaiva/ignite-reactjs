49aaecc487adc5a92805ba811724dc5d4abce1bf
lspaiva <lucasp4iva@gmail.com>
2022-03-08 13:12:45 -0300

    feat: adding component for modal + style

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,9 @@ import  Modal from 'react-modal';
 import { GlobalStyle } from './styles/global';
 import { Header } from './components/Header';
 import { Dashboard } from './components/Dashboard';
+import { NewTransactionModal } from './components/NewTransactionModal';
+
+Modal.setAppElement('#root');
 
 export function App() {
   const [isNewTransactionModalOpen, setNewTransactionModalOpen] = useState(false);
@@ -21,12 +24,10 @@ export function App() {
       <GlobalStyle />
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
       <Dashboard />
-      <Modal
+      <NewTransactionModal
         isOpen={isNewTransactionModalOpen}
-        onRequestClose={handleCloseNewTransactionModal}
-      >
-        <h2>Cadastrar transação</h2>
-      </Modal>
+        onRequestClose={handleCloseNewTransactionModal} 
+      />
     </>
   );
 }
