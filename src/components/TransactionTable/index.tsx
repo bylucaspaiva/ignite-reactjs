466113cebcea7ae7637135b3c65a0fa5da5ff9a7
lspaiva <lucasp4iva@gmail.com>
2022-03-09 12:46:14 -0300

    refactor: change how the list display value and date

--- src/components/TransactionTable/index.tsx
+++ src/components/TransactionTable/index.tsx
@@ -1,12 +1,25 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api } from "../services/api";
 import { Container } from "./styles";
 
-export function TransactionTable () {
+interface transaction {
+  id: number,
+  title: string,
+  amount: number,
+  type: string,
+  category: string,
+  createdAt: string,
+}
+
+
+export function TransactionTable() {
+  const [transactions, setTransactions] = useState<transaction[]>([]);
+
   useEffect(() => {
     api.get('transactions')
-      .then(response => console.log(response.data))
+      .then(response => setTransactions(response.data.transactions))
   }, []);
+
 
   return (
     <Container>
@@ -20,18 +33,24 @@ export function TransactionTable () {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de Website</td>
-            <td className="deposit">R$5.000</td>
-            <td>Desenvolvimento</td>
-            <td>07/03/2022</td>
-          </tr>
-          <tr>
-            <td>Aluguel</td>
-            <td className="withdraw">-R$1.200</td>
-            <td>Casa</td>
-            <td>07/03/2022</td>
-          </tr>
+          {transactions.map(transaction => {
+            return (
+              <tr key={transaction.id}>
+                <td>{transaction.title}</td>
+                <td className={transaction.type}>
+                  {new Intl.NumberFormat('pt-BR', {
+                    style: 'currency',
+                    currency: 'BRL'
+                  }).format(transaction.amount)}
+                </td>
+                <td>{transaction.category}</td>
+                <td>
+                  {new Intl.DateTimeFormat('pt-BR')
+                    .format(new Date(transaction.createdAt))}
+                </td>
+              </tr>
+            )
+          })}
         </tbody>
       </table>
     </Container>
--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { App } from './App';
-import { createServer } from 'miragejs'
+import { createServer, Model } from 'miragejs'
 
 createServer({
+  models: {
+    transaction: Model,
+  },
+
+  seeds(server) {
+    server.db.loadData({
+      transactions: [
+        {
+          id: 1,
+          title: 'Freelancer de web design',
+          type: 'deposit',
+          category: 'dev',
+          amount: 6000,
+          createdAt: new Date('2022-03-08 09:00:00'),
+        },
+        {
+          id: 2,
+          title: 'Freelancer de mobile',
+          type: 'withdraw',
+          category: 'dev',
+          amount: 12000,
+          createdAt: new Date('2022-03-08 06:00:00'),
+        },
+      ],
+    })
+  },
+
+
   routes() {
     this.namespace = 'api'
 
     this.get('/transactions', () => {
-      return [
-        {
-          id: 1,
-          title: 'transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createDate: new Date()
-        }
-      ]
+      return this.schema.all('transaction');
+    })
+
+    this.post('transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+      return schema.create('transaction', data);
     })
   }
 })
