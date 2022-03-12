d22e639411d33e9b6c5a1802e48d32f6aa4b0df0
bylucaspaiva <lucasp4iva@gmail.com>
2022-03-11 22:58:47 -0300

    style: adding global style to project

---
+++ 03-ignews/src/pages/index.tsx
@@ -0,0 +1,17 @@
+import Head from 'next/head'
+
+import styles from '../styles/global.scss'
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Início | ignews</title>
+      </Head>
+      <h1 className={styles.title}>
+      Hello World
+      </h1>
+    </>
+    
+  )
+}
