7c08b382d5d79b8cdede08547ce621892a63c7c6
bylucaspaiva <lucasp4iva@gmail.com>
2022-03-11 23:10:11 -0300

    style: adding media query to change size of fonts

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -1,15 +1,13 @@
 import Head from 'next/head'
 
-import styles from '../styles/global.scss'
-
 export default function Home() {
   return (
     <>
       <Head>
         <title>Início | ignews</title>
       </Head>
-      <h1 className={styles.title}>
-      Hello World
+      <h1>
+        Hello World
       </h1>
     </>
     
