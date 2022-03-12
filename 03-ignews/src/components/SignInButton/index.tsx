a79c7a6c49c490523aa807fe3bdc9603194577a0
bylucaspaiva <lucasp4iva@gmail.com>
2022-03-12 01:34:02 -0300

    feat: adding button component

---
+++ 03-ignews/src/components/SignInButton/index.tsx
@@ -0,0 +1,12 @@
+import {FaGithub} from 'react-icons/fa'
+
+import styles from './styles.module.scss'
+
+export function SignInButton () {
+  return (
+    <button
+     type="button"
+     className={styles.signInButton} ><FaGithub />Sign in with GitHub</button>
+
+  );
+}
