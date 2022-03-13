b9b30a1b2db8c7365968bd269935cd835b1084eb
bylucaspaiva <lucasp4iva@gmail.com>
2022-03-13 18:49:36 -0300

    feat: creating interface for Home component

--- 03-ignews/src/pages/index.tsx
+++ 03-ignews/src/pages/index.tsx
@@ -2,10 +2,18 @@ import { GetServerSideProps } from 'next'
 
 import Head from 'next/head'
 import { SubscribeButton } from '../components/SubscribeButton'
+import { stripe } from '../services/stripe'
 
 import styles from './home.module.scss'
 
-export default function Home() {
+interface HomeProps {
+  product: {
+    priceId: string;
+    amount: number;
+  }
+}
+
+export default function Home({product}: HomeProps) {
   return (
     <>
       <Head>
@@ -17,7 +25,7 @@ export default function Home() {
           <h1>News about the <span>react</span> world</h1>
           <p>
             Get access to all the publications <br/>
-            <span>for 9,90 for month</span>
+            <span>for {product.amount} for month</span>
           </p>
           <SubscribeButton />
         </section>
@@ -29,7 +37,19 @@ export default function Home() {
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
+  const price = await stripe.prices.retrieve('prod_LJJqBUwaBYxRFm', 
+  {
+    expand: ['product']
+  })
+
+  const product = {
+    priceId: price.id,
+    amount: (price.unit_amount / 100),
+  }
+  
   return {
-    
+    props: {
+      product,
+    }
   }
 }
