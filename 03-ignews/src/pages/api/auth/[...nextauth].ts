824117b0813f74a5ee0ed2121b9cf995f1f6f6cc
bylucaspaiva <lucasp4iva@gmail.com>
2022-03-15 16:23:59 -0300

    feat: saving email on faunadb

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -1,14 +1,34 @@
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
 
+import {fauna} from '../../../services/fauna'
+import {query as q} from 'faunadb'
+
 export default NextAuth({
   // Configure one or more authentication providers
   providers: [
     GithubProvider({
       clientId: process.env.GITHUB_ID,
       clientSecret: process.env.GITHUB_SECRET,
-      scope: 'read:user'
     }),
-    // ...add more providers here
   ],
+  callbacks: {
+    async signIn(user, account, profile) {
+
+      const {email} = user;
+
+      try {
+        await fauna.query(
+          q.Create(
+            q.Collection('users'),
+            {data: { email }}
+          )
+        )
+  
+        return true;
+      } catch {
+        return false;
+      }
+    },
+  }
 })
