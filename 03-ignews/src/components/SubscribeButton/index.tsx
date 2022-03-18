44112534516cb6033f9b06b76d67c643002ae7f3
bylucaspaiva <lucasp4iva@gmail.com>
2022-03-18 10:13:22 -0300

    feat: testing if user is logged in

--- 03-ignews/src/components/SubscribeButton/index.tsx
+++ 03-ignews/src/components/SubscribeButton/index.tsx
@@ -1,6 +1,18 @@
+import { useSession, signIn } from 'next-auth/react';
 import styles from './styles.module.scss';
 
 export function SubscribeButton () {
+  const {data: session} = useSession();
+  
+  function handleSubscribe () {
+    
+    if (!session) {
+      signIn('github')
+      return;
+    }
+
+    
+  }
   return (
     <button 
       type="button"
