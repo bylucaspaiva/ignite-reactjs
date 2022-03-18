94903a27bec1e4d4770c417cac7a67f042dab2db
bylucaspaiva <lucasp4iva@gmail.com>
2022-03-18 10:03:20 -0300

    feat: handle test duplicate email on fauna

--- 03-ignews/src/pages/api/auth/[...nextauth].ts
+++ 03-ignews/src/pages/api/auth/[...nextauth].ts
@@ -13,22 +13,41 @@ export default NextAuth({
     }),
   ],
   callbacks: {
-    async signIn(user, account, profile) {
-
+    async signIn({ user, account, profile }) {
+      
       const {email} = user;
 
       try {
         await fauna.query(
-          q.Create(
-            q.Collection('users'),
-            {data: { email }}
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(user.email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              {data: { email }}
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(user.email)
+            )
           )
         )
-  
+      )
+        console.log({email});
         return true;
-      } catch {
-        return false;
+      } catch (e) {
+          console.log(e);
+          return false;
       }
     },
   }
 })
+
+
